434f1e3da34385306b23f9085e75f6762031e080
easyandones <easyandones@gmail.com>
2025-04-10 23:22:40 -0700

    yarn add react-icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
 import { css } from "../../styled-system/css";
 
+import { FaCode } from "react-icons/fa";
+
 export default function Home() {
   return (
-    <div className={css({ fontSize: "10em", fontWeight: "bold" })}>
-      Hello World!
+    <div
+      className={css({
+        display: "flex",
+        fontSize: "10em",
+        fontWeight: "bold",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "0.1em",
+      })}
+    >
+      <FaCode />
+      <span>Hello World!</span>
     </div>
   );
 }
